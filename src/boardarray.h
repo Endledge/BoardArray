024bf6beb015a43eb4a4705ae1f33940794da19d
Clark Louise Francisco <clarkendledge051@gmail.com>
2025-09-25 14:13:22 +0800

    implement add() method for boardarray

--- src/boardarray.h
+++ src/boardarray.h
@@ -11,13 +11,38 @@ class BoardArray : public Board {
         BoardArray() {
             array = new Entry[SIZE];
             index = 0;
+        
         }
-
+        
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-        }
+            
+            for (int i = 0; i < index; i++) {
+                Entry current = array[i];
+                if (entry->compare(&current)) {
+                    for (int j = index; j > i; j--) {
+                        if (j == SIZE) continue;
+                        array[j] = array[j - 1];
+                    }
+                    array[i] = *entry;
+                    if (index < SIZE) index++;
+                    return;
+                }
+            }
 
+            if(index < SIZE) {
+                array[index] = *entry;
+                index++;
+            } else { 
+                if (entry->compare(&array[index - 1])) {
+                    array[index - 1] = *entry;
+                }
+                else {
+                    cout << entry->name << "'s score is too low to be added!" << endl;
+                }
+            }
+        }
         void print() {
             for (int i = 0; i < index; i++) {
                 cout << i + 1 << ". ";
